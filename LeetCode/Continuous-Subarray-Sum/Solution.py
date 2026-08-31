454dedd7f1a1cad22586bcf443f55a792b8c03ee
Tanya Diwedi <tanyadiwedi78@gmail.com>
2026-08-31 10:33:39 +0530

    Add solution for Continuous Subarray Sum

---
+++ LeetCode/Continuous-Subarray-Sum/Solution.py
@@ -0,0 +1,15 @@
+public boolean checkSubarraySum(final int[] nums, final int k) {
+    var sumsSet = new HashSet<Integer>(nums.length);
+    int sum = 0, prevSum;
+
+    for (int i = 0; i < nums.length; i++) {
+        prevSum = sum;
+        sum = (sum + nums[i]) % k;
+        if (sumsSet.contains(sum)) {
+            return true;
+        }
+        sumsSet.add(prevSum);
+    }
+
+    return false;
+}
